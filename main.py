f546777b7021f961f9c64d04bf0789903a37c429
sammytamimi <89275325+sammytamimi@users.noreply.github.com>
2025-07-20 17:02:49 +0100

    add posts

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from fastapi.params import Body
 from pydantic import BaseModel
 from typing import Optional
+from random import randrange
 
 app = FastAPI()
 
@@ -11,18 +12,21 @@ class Post(BaseModel):
     published: bool = True
     rating: Optional[int] = None
     
+my_posts = [{"title": "title of post 1", "content": "content of post 1", "id": 1}, {"title": "favourite foods", "content": "i like pizza", "id": 2}]
+    
 @app.get("/")
 async def root():
     return {"message": "Welcome to my API"}
 
 @app.get("/posts")
 def get_posts():
-    return {"data": "This is your post"}
+    return {"data": my_posts}
 
 @app.post("/posts")
 async def create_post(post: Post):
-    print(post)
-    print(post.model_dump())
-    return {"data": post}
+    post_dict = post.model_dump()
+    post_dict['id'] = randrange(0, 100000)
+    my_posts.append(post_dict)
+    return {"data": post_dict}
 
 # title str, content str
