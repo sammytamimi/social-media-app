f9ba54c1e78d95172200a882776df56bf976f98b
sammytamimi <89275325+sammytamimi@users.noreply.github.com>
2025-07-21 19:14:00 +0100

    add DELETE post function exception handling and support function. Also add correct response codes

--- main.py
+++ main.py
@@ -18,13 +18,21 @@ def find_post(id):
     for p in my_posts:
         if p["id"] == id:
             return p
+
+def find_index_post(id):
+    for i, p in enumerate(my_posts):
+        if p['id'] == id:
+            return i
+    return None
+        
+    
     
 @app.get("/")
 async def root():
     return {"message": "Welcome to my API"}
 
 @app.get("/posts")
-def get_posts():
+async def get_posts():
     return {"data": my_posts}
 
 @app.post("/posts", status_code=status.HTTP_201_CREATED) # change default status code to 201 from 200 to show post was created
@@ -42,4 +50,16 @@ async def get_post(id: int, response: Response):
         # response.status_code = status.HTTP_404_NOT_FOUND
         # return {'message' : f"post with id: {id} was not found "}
     return {"post_detail": post}
+
+@app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
+async def delete_post(id: int):
+    index = find_index_post(id)
+    if index is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Post with id: {id} not found")
+    
+    my_posts.pop(index)
+    
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
+    
+
     
