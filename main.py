878612c516ad9651ce51ddfa6c3091b48f716720
sammytamimi <89275325+sammytamimi@users.noreply.github.com>
2025-07-21 18:28:18 +0100

    clean up 404 error for GET operation on posts

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Response, status, HTTPException
 from fastapi.params import Body
 from pydantic import BaseModel
 from typing import Optional
@@ -13,6 +13,11 @@ class Post(BaseModel):
     rating: Optional[int] = None
     
 my_posts = [{"title": "title of post 1", "content": "content of post 1", "id": 1}, {"title": "favourite foods", "content": "i like pizza", "id": 2}]
+
+def find_post(id):
+    for p in my_posts:
+        if p["id"] == id:
+            return p
     
 @app.get("/")
 async def root():
@@ -29,4 +34,12 @@ async def create_post(post: Post):
     my_posts.append(post_dict)
     return {"data": post_dict}
 
-# title str, content str
+@app.get("/posts/{id}") # path parameter for post
+async def get_post(id: int, response: Response):
+    post = find_post(id)
+    if not post:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id: {id} was not found ")
+        # response.status_code = status.HTTP_404_NOT_FOUND
+        # return {'message' : f"post with id: {id} was not found "}
+    return {"post_detail": post}
+    
